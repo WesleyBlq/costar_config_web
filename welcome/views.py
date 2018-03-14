9eea468866059c518d0adfc6864d14e7be960699
mac bao <254284297@qq.com>
2018-03-14 17:13:05 +0800

    ui desgin

--- welcome/views.py
+++ welcome/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     title="costar系统配置"
-    return render(request, 'welcome.html', {'title': title,})
+    host_ip = "192.168.0.100"
+    UPS_ip = "192.168.0.98"
+    return render(request, 'welcome.html', {'title': title, "host_ip": host_ip, "UPS_ip": UPS_ip})
