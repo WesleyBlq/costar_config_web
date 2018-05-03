a17f12ae14352510eb70a70065408044b64a6313
mac bao <254284297@qq.com>
2018-05-03 18:11:27 +0800

    add camera type config

---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sleep 3
+/usr/bin/python3 ./manage.py runserver 0.0.0.0:80
--- welcome/views.py
+++ welcome/views.py
@@ -15,27 +15,34 @@ def welcome(request):
     host_ip = cfg.get("server", "ip")
     gateway = cfg.get("server", "gateway")
     UPS_ip = cfg.get("server", "ups_ip")
+    camera_type = cfg.get("server", "camera_type")
 
     return render(request, 'welcome.html', {'title': title, "host_ip": host_ip, \
-        "UPS_ip": UPS_ip, "gateway": gateway})
+        "UPS_ip": UPS_ip, "gateway": gateway, "camera_type": camera_type})
 
 def change(request):
     host_ip = request.GET["host_ip"]
     UPS_ip = request.GET["UPS_ip"]
     gateway = request.GET["gateway"]
-    
+    camera_type = request.GET["camera_type"]
 
     cfg = configparser.ConfigParser()
     cfg.read(config_file_path, encoding = 'utf-8-sig')
     cfg.set("server", "ups_ip", UPS_ip)
     cfg.set("server", "ip", host_ip)
     cfg.set("server", "gateway", gateway)
+    cfg.set("server", "camera_type", camera_type)
     cfg.write(open(config_file_path, "w"))
     
+    # 修改linux系统固定IP
+    content = None
     with open('/etc/network/interfaces') as fh:
         content = fh.read()
-        content = re.sub("address " + r'*.*.*.*', "adress 192.168.100.100", content)
-        print(content)
-    # call('restart')
+        content = re.sub("address " + r'*.*.*.*', "address " + host_ip, content)
+        content = re.sub("gateway " + r'*.*.*.*', "gateway " + gateway, content)
+
+    with open('/etc/network/interfaces', 'w') as fw:
+        fw.write(content)
+    call('reboot')
 
     return HttpResponse("success")
