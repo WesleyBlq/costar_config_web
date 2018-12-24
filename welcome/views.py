1b436828ad9f7f1c6cacadf5694193f3db6ead68
mac bao <254284297@qq.com>
2018-12-24 11:24:31 +0800

    add change mac func

--- welcome/views.py
+++ welcome/views.py
@@ -13,6 +13,8 @@ def welcome(request):
     cfg = configparser.ConfigParser()
     cfg.read(config_file_path, encoding = 'utf-8-sig')
     host_ip = cfg.get("server", "ip")
+    mac_addr = cfg.get("server", "mac_addr")
+    
     gateway = cfg.get("server", "gateway")
     UPS_ip = cfg.get("server", "ups_ip")
     camera_type = cfg.get("server", "camera_type")
@@ -21,7 +23,8 @@ def welcome(request):
 
     return render(request, 'welcome.html', {'title': title, "host_ip": host_ip, \
         "UPS_ip": UPS_ip, "gateway": gateway, "camera_type": camera_type, \
-        "default_focus_speed": default_focus_speed, "serial_num": serial_num})
+        "default_focus_speed": default_focus_speed, "serial_num": serial_num, \
+        "mac_addr": mac_addr})
 
 def change(request):
     host_ip = request.GET["host_ip"]
@@ -29,6 +32,7 @@ def change(request):
     gateway = request.GET["gateway"]
     camera_type = request.GET["camera_type"]
     serial_num = request.GET["serial_num"]
+    mac_addr = request.GET["mac_addr"]
 
     cfg = configparser.ConfigParser()
     cfg.read(config_file_path, encoding = 'utf-8-sig')
@@ -37,6 +41,7 @@ def change(request):
     cfg.set("server", "gateway", gateway)
     cfg.set("server", "camera_type", camera_type)
     cfg.set("server", "serial_num", serial_num)
+    cfg.set("server", "mac_addr", mac_addr)
     cfg.write(open(config_file_path, "w"))
     
     # 修改linux系统固定IP
@@ -47,6 +52,21 @@ def change(request):
         content = re.sub("gateway " + r'*.*.*.*', "gateway " + gateway, content)
 
     with open('/etc/network/interfaces', 'w') as fw:
+        fw.write(content)
+
+    content = None
+    with open('/etc/init.d/S50costar') as fh:
+        content = fh.read()
+        print("########")
+        print(content)
+        content = re.sub(r'''([0-9A-F]{1,2}[:]){5}([0-9A-F]{1,2})''',
+                        mac_addr,
+                        content,)
+
+        print("########")
+        print(content)
+
+    with open('/etc/init.d/S50costar', 'w') as fw:
         fw.write(content)
     
     call('reboot')
