3a6bb684f1b1847e44e2ee13b0cd86fa3a9a4115
mac bao <254284297@qq.com>
2019-02-25 17:33:52 +0800

    flush some change

--- welcome/views.py
+++ welcome/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import configparser
 from subprocess import call
 import re
+import datetime
 
 config_file_path = "../bhw_cos_gateway/config.ini"
 
@@ -20,19 +21,30 @@ def welcome(request):
     camera_type = cfg.get("server", "camera_type")
     serial_num = cfg.get("server", "serial_num")
     default_focus_speed = cfg.get("camera", "default_focus_speed")
+    infra_open = cfg.get("timing", "infra_open")
+    infra_close = cfg.get("timing", "infra_close")
+
+    # ISOTIMEFORMAT = 
+    sys_time = datetime.datetime.now().strftime('%H:%M')
 
     return render(request, 'welcome.html', {'title': title, "host_ip": host_ip, \
         "UPS_ip": UPS_ip, "gateway": gateway, "camera_type": camera_type, \
         "default_focus_speed": default_focus_speed, "serial_num": serial_num, \
-        "mac_addr": mac_addr})
+        "mac_addr": mac_addr, "infra_open": infra_open, "infra_close": infra_close, "sys_time": sys_time})
 
 def change(request):
+    # return 
+
     host_ip = request.GET["host_ip"]
     UPS_ip = request.GET["UPS_ip"]
     gateway = request.GET["gateway"]
     camera_type = request.GET["camera_type"]
     serial_num = request.GET["serial_num"]
     mac_addr = request.GET["mac_addr"]
+    sys_time = request.GET["sys_time"]
+    infra_open = request.GET["infra_open"]
+    infra_close = request.GET["infra_close"]
+    
 
     cfg = configparser.ConfigParser()
     cfg.read(config_file_path, encoding = 'utf-8-sig')
@@ -42,6 +54,9 @@ def change(request):
     cfg.set("server", "camera_type", camera_type)
     cfg.set("server", "serial_num", serial_num)
     cfg.set("server", "mac_addr", mac_addr)
+    cfg.set("timing", "infra_open", infra_open)
+    cfg.set("timing", "infra_close", infra_close)
+
     cfg.write(open(config_file_path, "w"))
     
     # 修改linux系统固定IP
@@ -69,6 +84,10 @@ def change(request):
     with open('/etc/init.d/S50costar', 'w') as fw:
         fw.write(content)
     
+    cmd = "date -s " + sys_time
+    import os
+    os.system("date -s " + sys_time)
+    os.system("hwclock -w")
     call('reboot')
 
     return HttpResponse("success")
